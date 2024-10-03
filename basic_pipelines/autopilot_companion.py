ef40035fca2d65ab258fd91d06bb535ea47fec5b
KoBoh <konstboh@gmail.com>
2024-10-03 11:57:21 +0300

    feat: fix yaw, pitch, roll

--- basic_pipelines/autopilot_companion.py
+++ basic_pipelines/autopilot_companion.py
@@ -148,32 +148,37 @@ class AutipilotCompanion:
             boxes (dict): list of bboxes in the format [xmin,ymin.xmax,ymax]
             frame_size (tuple): a tuple of w and h (w,h)
         """
-
+        # checking if the locked object id in the current detections
         if self.target_id_locked in class_ids.keys():
-
+            
+            # calculating x, y bias 
             target_xy_bias = self.xy_bias(
                 tuple((float(i) for i in boxes[self.target_id_locked])))
-            print(target_xy_bias)
+            print(f"Bias xy: {target_xy_bias}")
 
-            # TODO calculate angles here
+            # calculating angles in radians
             velocities = self.visual_pid_wrapper(
-                target_xy_bias[0], target_xy_bias[1])       
+                target_xy_bias[0], target_xy_bias[1])    
             
-            
-            print(velocities)
+            print(f"Velocities yaw, pitch, roll: {velocities}")
+
+            # set flight angles
             self.__set_attitude_target(
-                velocities['roll'], velocities['pitch'], velocities['yaw'], velocities['throttle'])
+                roll=velocities['roll'],
+                pitch=velocities['pitch'],
+                yaw=velocities['yaw'],
+                throttle=velocities['throttle'])
 
         else:
             # if detection.get_confidence() < ac.min_confidence:
             #     continue
-            self.target_id_locked = 8
+            self.target_id_locked = 10
 
     @staticmethod
     def xy_bias(bbox_xyxy: tuple[float, float, float, float], w=1.0, h=1.0) -> list:
         return [(bbox_xyxy[0] + bbox_xyxy[2]) / 2 - (w / 2), (bbox_xyxy[1] + bbox_xyxy[3]) / 2 - (h / 2)]
 
-    def visual_pid_wrapper(self, x_bias, y_bias) -> list:
+    def visual_pid_wrapper(self, x_bias, y_bias) -> dict:
 
         velocities = {
             'roll': 0,
@@ -191,38 +196,49 @@ class AutipilotCompanion:
         
         velocities['yaw'] = pid_velocities[0]
         velocities['pitch'] = pid_velocities[1]
+        velocities['roll'] = pid_velocities[2]
         
 
         return velocities
 
-    def __set_attitude_target(self, roll, pitch, yaw, throttle=0, tolerance=10):
+    def __set_attitude_target(self, roll: float, pitch: float, yaw: float, throttle=0, tolerance=10):
+        """Set flight parameters for a plane: yaw, pitch, roll, throttle
+
+        Args:
+            roll (float): roll angle in rad
+            pitch (float): pitch angle in rad
+            yaw (float): yaw angle in rad
+            throttle (int, optional): a throttle level. Defaults to 0.
+            tolerance (int, optional): a tolerance level (is not used in the current implementation). Defaults to 10.
+        """
 
         try:
-            m = self.master.recv_match(type='ATTITUDE',
-                                       blocking=True,
-                                       timeout=0.1)
+            m = self.master.recv_match(
+                type='ATTITUDE',
+                blocking=True,
+                timeout=0.1)
+            
             if m is None:
                 return
 
             # attitude in radians:
-            q = quaternion.Quaternion([math.radians(roll),
-                                       math.radians(pitch),
-                                       math.radians(yaw)])
-            self.master.mav.set_attitude_target_send(int(1e3 * (time.time() - self.boot_time)),
-                                                     self.master.target_system,
-                                                     self.master.target_component,
-                                                     self.typemask_ypr_throttle,
-                                                     q,
-                                                     0,  # roll rate, not used in AP
-                                                     0,  # pitch rate, not used in AP
-                                                     0,  # yaw rate, not used in AP
-                                                     throttle)
+            q = quaternion.Quaternion([roll, pitch, yaw])
+            self.master.mav.set_attitude_target_send(
+                int(1e3 * (time.time() - self.boot_time)),
+                self.master.target_system,
+                self.master.target_component,
+                self.typemask_ypr_throttle,
+                q,
+                0,  # roll rate, not used in AP
+                0,  # pitch rate, not used in AP
+                0,  # yaw rate, not used in AP
+                throttle)
         except Exception as e:
             print(
                 f"Error setting attitude: \n   - roll: {roll}\n   - pitch: {pitch}\n   - yaw: {yaw}\n   - throttle: {throttle} \n {e}")
 
     def __calculate_velocities(self, erx: float, ery: float, erz=0.0, w=1, h=1) -> tuple:
-        """Calculates required velocities among x, y, z axis
+        """Calculates required velocities among x (yaw), y (pitch), z (roll) axis
 
         Args:
             erx (int): target direction error among x
@@ -230,7 +246,7 @@ class AutipilotCompanion:
             ery (int): target direction error among z
 
         Returns:
-            tuple: tuple of required velocities among x, y, z axis (velocity_x, velocity_y, velocity_z)
+            tuple: tuple of required velocities among x, y, z axis (yaw, pitch, roll)
         """
 
         try:
@@ -246,9 +262,9 @@ class AutipilotCompanion:
                 self.vertical_fov,
             )
             return (
-                velocity.control_output_x,
-                velocity.control_output_y,
-                velocity.control_output_z,
+                velocity.control_output_x, # yaw
+                velocity.control_output_y, # pitch
+                velocity.control_output_z, # roll
             )
         except Exception as er:
             print(f"Error calling shared lib: {er}")
